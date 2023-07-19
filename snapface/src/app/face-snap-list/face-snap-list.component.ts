6196c5033cd980119db520fe0b87b6d028e318f9
emmanuelle <emm.floret@gmail.com>
2023-07-19 12:41:03 +0200

    evolution1

--- snapface/src/app/face-snap-list/face-snap-list.component.ts
+++ snapface/src/app/face-snap-list/face-snap-list.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import{FaceSnap} from'../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap-list',
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
-export class FaceSnapListComponent {
+export class FaceSnapListComponent implements OnInit{
+
+  faceSnaps: FaceSnap[] =[
+    {
+      id: 1,
+      title:"Mon premier facesnap",
+      description: "description du facesnap",
+      createDate: new Date(),
+      snaps: 200,
+      imageUrl:" "
+
+    },
+    {
+      id: 2,
+      title:"Mon second facesnap",
+      description: "description blablka du facesnap",
+      createDate: new Date(),
+      snaps: 0,
+      imageUrl:" ",
+      location: "New York"
+      
+    }
+  ]
+  constructor(){
+
+  }
+  ngOnInit(): void{}
 
 }
--- snapface/src/app/single-face-snap/single-face-snap.component.ts
+++ snapface/src/app/single-face-snap/single-face-snap.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
@@ -8,27 +8,8 @@ import { FaceSnap } from '../models/face-snap.model';
 })
 export class SingleFaceSnapComponent implements OnInit {
 
-  faceSnaps: FaceSnap[] =[
-    {
-      id: 1,
-      title:"Mon premier facesnap",
-      description: "description du facesnap",
-      createDate: new Date(),
-      snaps: 200,
-      imageUrl:" "
+  @Input() faceSnap!: FaceSnap;
 
-    },
-    {
-      id: 2,
-      title:"Mon second facesnap",
-      description: "description blablka du facesnap",
-      createDate: new Date(),
-      snaps: 0,
-      imageUrl:" ",
-      location: "New York"
-      
-    }
-  ]
   constructor(){}
 
   ngOnInit(): void{}
